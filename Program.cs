77dfe733eaa2fdc7094ecb9834102a7abb382510
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-03-11 11:20:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words;
+            using (StreamReader sr = new StreamReader("Inlet.in"))
+            {
+                words = sr.ReadToEnd().Trim().Split(' ');
+            }
+            string result = string.Empty;
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] tmp = words[i].ToCharArray();
+                for (int j = 1; j < tmp.Length; j++)
+                {
+                    if (tmp[j] == tmp[0])
+                    {
+                        tmp[j] = '_';
+                    }
+                }
+                result += new string(tmp) + " ";
+            }
+            File.WriteAllText("Outlet.out", result);
+        }
+    }
+}
